1a2137c6f3491f3d9e2a48b813b40e988ecd4f42
Joshua Blevins <josh.adam.blevins@gmail.com>
2023-12-07 04:18:30 -0500

    feature: added files

---
+++ src/app/page.tsx
@@ -0,0 +1,64 @@
+"use client";
+import * as React from "react";
+import QrCodeReact from 'react-qr-code'
+import QRCode from "qrcode";
+import { encode } from "base-64";
+
+export default function Home() {
+  const [qrCodeContent, setQrCodeContent] = React.useState("");
+  const [qrCodeImageData, setQrCodeImageData] = React.useState("");
+
+  function handleQrCodeContentOnChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setQrCodeContent(event.currentTarget.value);
+  }
+
+  React.useEffect(() => {
+    QRCode.toDataURL(qrCodeContent, {
+      type: "png"
+    }, function (error: Error, url: string) {
+      if (error) {
+        console.error(error);
+      }
+
+      console.log('url: ', url);
+
+      setQrCodeImageData(url);
+    });
+  }, [qrCodeContent])
+
+  return (
+    <main className="container my-4 mx-auto flex flex-col md:flex-row gap-2">
+      <form className="w-full md:w-2/3 p-4">
+        <label className="block w-full font-sans font-bold text-xl">
+          QR Code Content
+
+          <textarea
+            className="block w-full font-sans font-regular text-base text-black p-2 my-2"
+            name="qr-code-content"
+            rows={10}
+            placeholder="Type content here and it is stuffed into the QR Code."
+            onChange={handleQrCodeContentOnChange}
+          />
+        </label>
+      </form>
+
+      <div className="mt-8">
+        <QrCodeReact
+          value={qrCodeContent}
+        />
+
+        <a
+          aria-disabled={!!qrCodeImageData}
+          className="inline-block my-2 hover:text-blue-300"
+          href={`data:image/png;base64${qrCodeImageData}`}
+          download={"QR-Code"}
+          type=""
+          target="_blank"
+          rel="noreferrer"
+        >
+          Download QR Code Image
+        </a>
+      </div>
+    </main>
+  )
+}
